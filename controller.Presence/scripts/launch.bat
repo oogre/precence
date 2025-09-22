e2e95b9f80342349049575611f8944cfcf965ad3
Your Name <you@example.com>
2025-09-22 17:39:29 +0200

    auto launch

--- controller.Presence/scripts/launch.bat
+++ controller.Presence/scripts/launch.bat
@@ -1,12 +1,25 @@
 
+timeout /t 10 /nobreak
+
+
 tasklist /FI "IMAGENAME eq obs64.exe" 2>NUL | find /I /N "obs64.exe">NUL
 
 IF NOT "%ERRORLEVEL%" == "0"  (
-	cd /D "C:\Program Files\obs-studio\bin\64bit"
-	start "" "obs64.exe"
-	sleep(3000)
+	start start /b obs64.exe.lnk
+	timeout /t 10 /nobreak
 )
 
-cd /D %~dp0
-cd /D ../
-node --trace-warnings ./release/main.js
+cd ../
+@echo off
+echo. >> ./data/logs/logfile.log
+echo. >> ./data/logs/logfile.log
+echo. >> ./data/logs/logfile.log
+echo #################################### >> ./data/logs/logfile.log
+echo #################################### >> ./data/logs/logfile.log
+echo #################################### >> ./data/logs/logfile.log
+echo RUN LAUNCH AT %date% %time%>> ./data/logs/logfile.log
+echo #################################### >> ./data/logs/logfile.log
+echo #################################### >> ./data/logs/logfile.log
+echo #################################### >> ./data/logs/logfile.log
+
+node --trace-warnings ./release/main.js >> ./data/logs/logfile.log 2>>&1
