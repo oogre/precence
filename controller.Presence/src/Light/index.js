fb28d53e09c851d606fce510ce41099cb6d84100
Matière humaine <matiere@humaine.tv>
2025-10-06 15:02:39 +0200

    lightController

--- controller.Presence/src/Light/index.js
+++ controller.Presence/src/Light/index.js
@@ -1,14 +1,12 @@
 import Enum from 'enum';
 import { wait, EventManager} from '../common/Tools.js';
 import { Client as OSC_Client } from 'node-osc';
-
-
 import { 
 	connectOutput as MidiOutConnect,
 	connectInput as MidiInConnect,
 	sendCC as MidiSendCC,
 } from './Midi/MidiTools.js';
-
+import { ChannelStatus as Channel_Status, nextChannel } from '../common/Constants.js';
 
 const knobs = [{
 	name : "pos",
@@ -24,29 +22,61 @@ const knobs = [{
 	value : 127
 }];
 
-
 export default class LightController extends EventManager {
 	static LightStatus = new Enum(['NOT_CONNECTED', "CONNECTING", "CONNECTED", 'ERROR']);
-	
+	static ChannelStatus = Channel_Status; 
 	constructor(conf){
 		super("LightController", ["request", "connect", "ready"]);
 		this.conf = conf;
-		this.conf.status = LightController.LightStatus.NOT_CONNECTED;
+		this.log = conf.log;
 		this.oscClient = new OSC_Client(conf.host, conf.port);
+		this.conf.status = LightController.LightStatus.CONNECTED;
+		this._mode = LightController.ChannelStatus.NONE;
 
-		this.displayInterface = MidiOutConnect(conf.name);
-		this.midiInterface = MidiInConnect(conf.name);
-		this.midiInterface.onCC((channel, number, value, deltaTime)=>{
-			if(!knobs[number]){
-				return;
-			}
-			knobs[number].value += value - 64
-			knobs[number].value = Math.min(128, Math.max(0, knobs[number].value));
-			MidiSendCC(this.displayInterface, 0, number, knobs[number].value);
-			this.oscClient.send(`/${knobs[number].name}`, knobs[number].value, (err) => {
-				if (err) console.error(err);
+		try{
+			this.displayInterface = MidiOutConnect(conf.name);
+			this.midiInterface = MidiInConnect(conf.name);
+			this.midiInterface.onCC((channel, number, value, deltaTime)=>{
+				if(!knobs[number]){
+					return;
+				}
+				knobs[number].value += value - 64
+				knobs[number].value = Math.min(128, Math.max(0, knobs[number].value));
+				MidiSendCC(this.displayInterface, 0, number, knobs[number].value);
+				this.oscClient.send(`/${knobs[number].name}`, knobs[number].value, (err) => {
+					if (err) console.error(err);
+				});
+
+				this.isRecordMode && this.trigger("request", {
+					name : `/${knobs[number].name}`, 
+					value : knobs[number].value
+				});
 			});
+		}catch(error){
+			this.log(error);
+		}
+	}
+
+	inject({name, value}){
+		this.oscClient.send(name, value, (err) => {
+			if (err) console.error(err);
 		});
+	}
+
+	nextMode(){
+		this._mode = nextChannel(this._mode);
+	}
+	get mode(){
+		return this._mode.value;
+	}
+	get isRecordMode(){
+		return this._mode == LightController.ChannelStatus.RECORD;
+	}
+	get isPlayMode(){
+		return this._mode == LightController.ChannelStatus.PLAY;
+	}
+	get isNoneMode(){
+		return this._mode == LightController.ChannelStatus.NONE;
 	}
 
 }
